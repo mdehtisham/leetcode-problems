28ae4354e17c1fc6cfcd90d0f5e42b5b67106be0
Md. Ehtisham Farooqui <mefxe01@gmail.com>
2021-07-09 23:52:10 +0530

    solved group anagram problem

--- 49. Group Anagrams/index.js
+++ 49. Group Anagrams/index.js
@@ -1,3 +1,19 @@
 let groupAnagrams = function (strs) {
-
+  let group = {};
+  for (let str of strs) {
+    const key = str.split('').sort().join()
+    if (!group[key]) {
+      group[key] = [];
+    }
+    group[key].push(str);
+  }
+  return Object.values(group);
 };
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+// Output: [ [ 'eat', 'tea', 'ate' ], [ 'tan', 'nat' ], [ 'bat' ] ] 
+console.log(groupAnagrams([""]));
+// Output: [['']] 
+console.log(groupAnagrams(["a"]));
+// Output: [ ['a'] ]
+
